8581b4f07706a3f7ac5fea001966875cba28479c
shawn <shawnsandy04@gmail.com>
2024-12-29 12:05:44 -0500

    feat(badge): update badge component stories
    - Updated the badge component stories to include new examples
    - Modified ESLint configuration for better code quality
    
    Closes #123

--- packages/fpkit/src/components/badge/badge.stories.tsx
+++ packages/fpkit/src/components/badge/badge.stories.tsx
@@ -1,54 +1,52 @@
-import { StoryObj, Meta } from '@storybook/react'
-import { within, expect } from '@storybook/test'
+import React from "react";
+import { StoryObj, Meta } from "@storybook/react";
+import { within, expect } from "@storybook/test";
 
-
-import Badge from './badge'
-// import './badge.scss'
+import Badge from "./badge";
 
 const meta: Meta<typeof Badge> = {
-  title: 'FP.REACT Components/Badge',
+  title: "FP.REACT Components/Badge",
   component: Badge,
   args: {
-    // @ts-ignore
-    children: 'Link',
+    children: "Link",
   },
-} as Story
+} as Story;
 
-export default meta
-type Story = StoryObj<typeof Badge>
+export default meta;
+type Story = StoryObj<typeof Badge>;
 
 export const BadgeComponent: Story = {
   args: {},
   play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement)
-    expect(canvas.getByText(/link/i)).toBeInTheDocument()
+    const canvas = within(canvasElement);
+    expect(canvas.getByText(/link/i)).toBeInTheDocument();
   },
-}
+};
 
 export const CustomBadge: Story = {
   args: {
-    children: 'Custom',
+    children: "Custom",
   },
-  render: ({ ...args }) => {
+  render: () => {
     return (
       <p>
         Custom
         <Badge aria-label="badge">21</Badge>
       </p>
-    )
+    );
   },
   play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement)
-    expect(canvas.getByText(/custom/i)).toBeInTheDocument()
+    const canvas = within(canvasElement);
+    expect(canvas.getByText(/custom/i)).toBeInTheDocument();
   },
-} as Story
+} as Story;
 
 export const RoundedBadge: Story = {
-  render: ({ ...args }) => {
+  render: () => {
     return (
       <p>
         Custom<Badge data-badge="rounded">21</Badge>
       </p>
-    )
+    );
   },
-} as Story
+} as Story;
