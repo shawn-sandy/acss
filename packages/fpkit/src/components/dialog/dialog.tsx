fc1b6fec2db0cf9c25cb5544c97bbc667edcfb50
shawn <shawnsandy04@gmail.com>
2024-12-30 23:31:18 -0500

    feat(dialog): add alert dialog component
    - created dialog.tsx for the alert dialog component
    - added dialog.stories.tsx to document the component in Storybook
    
    closes #123

---
+++ packages/fpkit/src/components/dialog/dialog.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import UI from "#components/ui";
+
+export type DialogProps = {
+  isOpen: boolean;
+  onClose?: () => void;
+  onOpen?: () => void;
+  children: React.ReactNode;
+};
+
+const Dialog = ({
+  isOpen = true,
+  onOpen,
+  onClose,
+  children,
+  ...props
+}: DialogProps): JSX.Element => {
+  const dialogRef = React.useRef<HTMLDialogElement>(null);
+  if (!isOpen) return <></>;
+
+  if (onClose) {
+    onClose();
+    dialogRef.current?.close();
+  }
+
+  if (onOpen) {
+    dialogRef.current?.showModal();
+    onOpen();
+  }
+
+  return (
+    <UI as="dialog" open={isOpen} ref={dialogRef} {...props}>
+      {children}
+    </UI>
+  );
+};
+
+export default React.memo(Dialog);
+Dialog.displayName = "Dialog";
