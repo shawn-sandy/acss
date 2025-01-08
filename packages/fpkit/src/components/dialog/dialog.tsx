efca0fd62d1e20667a9f81d2b19402c5189734cf
shawn <shawnsandy04@gmail.com>
2025-01-08 17:38:27 -0500

    feat(dialog): refactor dialog component structure for improved readability

--- packages/fpkit/src/components/dialog/dialog.tsx
+++ packages/fpkit/src/components/dialog/dialog.tsx
@@ -1,6 +1,5 @@
 import React, { useRef, useEffect } from "react";
 import UI from "#components/ui";
-import Button from "#components/buttons/button";
 import DialogHeader from "#components/dialog/views/dialog-header";
 import DialogFooter from "#components/dialog/views/dialog-footer";
 
@@ -76,42 +75,31 @@ export const Dialog: React.FC<DialogModalProps> = ({
   };
 
   return (
-    <>
-      <Button
-        type="button"
-        onClick={() => {
-          setIsOpen(true);
-        }}
-        data-btn="sm"
-      >
-        Open Dialog
-      </Button>
-      <UI
-        as="dialog"
-        role={isAlertDialog ? "alertdialog" : "dialog"}
-        ref={dialogRef}
-        onClose={handleClose}
-        onClick={handleClick}
-        aria-modal={isOpen ? "true" : undefined}
-        className="dialog-modal"
-      >
-        <DialogHeader dialogTitle={title} onClick={handleClose} />
+    <UI
+      as="dialog"
+      role={isAlertDialog ? "alertdialog" : "dialog"}
+      ref={dialogRef}
+      onClose={handleClose}
+      onClick={handleClick}
+      aria-modal={isOpen ? "true" : undefined}
+      className="dialog-modal"
+    >
+      <DialogHeader dialogTitle={title} onClick={handleClose} />
 
-        <UI
-          as="section"
-          className={`dialog-content ${className}`}
-          onClick={(e: React.MouseEvent) => e.stopPropagation()}
-        >
-          {children}
-          <DialogFooter
-            onClose={handleClose}
-            onConfirm={onConfirm}
-            confirmLabel={confirmLabel}
-            cancelLabel={cancelLabel}
-          />
-        </UI>
+      <UI
+        as="section"
+        className={`dialog-content ${className}`}
+        onClick={(e: React.MouseEvent) => e.stopPropagation()}
+      >
+        {children}
+        <DialogFooter
+          onClose={handleClose}
+          onConfirm={onConfirm}
+          confirmLabel={confirmLabel}
+          cancelLabel={cancelLabel}
+        />
       </UI>
-    </>
+    </UI>
   );
 };
 export default Dialog;
