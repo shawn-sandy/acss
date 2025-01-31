8be95119de9b13096fa9d67a919cfb69385fdfd8
shawn <shawnsandy04@gmail.com>
2025-01-31 16:55:41 -0500

    fix(dismiss-button): update storybook title to reflect correct element categorization

--- packages/fpkit/src/components/alert/elements/dismiss-button.stories.tsx
+++ packages/fpkit/src/components/alert/elements/dismiss-button.stories.tsx
@@ -3,7 +3,7 @@ import { within, expect } from "@storybook/test";
 import DismissButton from "./dismiss-button";
 
 const meta: Meta<typeof DismissButton> = {
-  title: "FP.REACT Components/DismissButton",
+  title: "FP.REACT Elements/DismissButton",
   component: DismissButton,
   tags: ["new"],
   parameters: {
